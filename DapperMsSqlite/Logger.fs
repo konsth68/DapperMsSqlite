2b7e85635f50dd8122114698cd986dafd94df7d8
Konstantin Dobkin <konsth68@gmail.com>
2025-10-24 16:08:46 +0300

    Add project files.

---
+++ DapperMsSqlite/Logger.fs
@@ -0,0 +1,25 @@
+﻿namespace DapperMsSqlite
+
+open Serilog
+
+type LogType =
+    | Console
+    | File
+
+
+[<RequireQualifiedAccess>]
+module DpMsSqlLog =
+
+
+    let loggerFileConfig =
+        LoggerConfiguration().MinimumLevel.Debug().WriteTo.File(".\DapperMsSqlite.log").CreateLogger()
+
+    let loggerConsoleConfig =
+        LoggerConfiguration().MinimumLevel.Debug().WriteTo.Console().CreateLogger()
+
+    let mutable CLog = loggerFileConfig
+
+    let loggerSetup (l: LogType) =
+        match l with
+        | LogType.Console -> CLog <- loggerConsoleConfig
+        | LogType.File -> CLog <- loggerFileConfig
